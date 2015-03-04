f588fa18e50816904092245edce934b395b07a61
Alejandro Acosta <aacosta@fedoraproject.org>
2015-03-03 19:17:40 -0700

    A few insignificant changes

--- isphpoptimal.sh
+++ isphpoptimal.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "THIS PROGRAM IS STILL UNFINISHED - DON'T TRY TO RUN IT UNTIL A DECENT RELEASE"
+
+
 INFILE=$1
 OUTFILE=$2
 KEYFILE=$3
@@ -14,19 +17,25 @@ fi
 
 
 
-#Check to make sure that infile exists
+#Check to make sure that both infile and keyfile exists
 
 if [ ! -f $INFILE ] ; then
    echo "Error... specified input file does not exist"
    exit 1
 fi
 
-
+#I'm pretty sure this could have been done in a single block, I'll fix it later
+if [ ! -f $KEYFILE ] ; then
+   echo "Error... specified key-value file does not exist"
+   exit 1
+fi
 
 
 
 echo "first arg: ${INFILE}"
 echo "second arg: ${OUTFILE}"
 echo "third arg: ${KEYFILE}"
+
+#Open KEYFILE and store keys and values into an array
 
 
